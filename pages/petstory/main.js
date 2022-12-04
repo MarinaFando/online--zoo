5bbd762bf363bb7cfa851735619fa053678eafb7
Marina Fando <marinafando@gmail.com>
2022-12-04 16:54:43 +0100

    feat: add popups in testimonials

--- pages/petstory/main.js
+++ pages/petstory/main.js
@@ -1,31 +1,32 @@
 function showBurgerMenu() {
-    const burgerMenuList = document.querySelector(".burger-container");
-    const greyArea = document.querySelector(".grey-area");
-  if (burgerMenuList.style.display === "flex") {
-    burgerMenuList.style.display = "none";
-
-  } else {
-    burgerMenuList.style.display = "flex";
-    greyArea.style.display = "block"
-  }
+  const burgerMenuList = document.querySelector(".burger-container");
+  const greyArea = document.querySelector(".grey-area");
+  burgerMenuList.style.display = "flex";
+  greyArea.style.display = "block"
 }
 
 function closeBurgerMenu() {
-    const burgerMenuList = document.querySelector(".burger-container");
-    const greyArea = document.querySelector(".grey-area");
-  if (burgerMenuList.style.display === "flex") {
-    burgerMenuList.style.display = "none";
-    greyArea.style.display = "none";
-  } else {
-    burgerMenuList.style.display = "flex";
-  }
+  const burgerMenuList = document.querySelector(".burger-container");
+  const greyArea = document.querySelector(".grey-area");
+  burgerMenuList.style.display = "none";
+  greyArea.style.display = "none";
 }
 
-function showSlides() {
-  let cards = document.querySelectorAll(".chooseanimal-card");
-  cards.forEach(
-    (card) => {
-      card.style.order = Math.floor(Math.random() * 7)
-    }
-  )
+function showPopup(id) {
+  const testimonialsPopupCard = document.querySelector(`#${id}`);
+  const greyArea = document.querySelector(".grey-popuparea");
+  testimonialsPopupCard.style.display = "flex";
+  greyArea.style.display = "block";
+}
+
+function closePopup(id) {
+  const testimonialsPopupCard = document.querySelector(`#${id}`);
+  const greyArea = document.querySelector(".grey-popuparea");
+  testimonialsPopupCard.style.display = "none";
+  greyArea.style.display = "none";
+}
+
+function closeAllPopups() {
+  document.querySelectorAll(".popupcard-container")
+  .forEach((popup) => closePopup(popup.id));
 }
